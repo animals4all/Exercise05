43ae1e52fd80ed654a20efefef24f997fcc1839d
Caroline <animals4all@gmail.com>
2015-07-02 12:01:59 -0400

    Count Letters in File

--- lettercount.py
+++ lettercount.py
@@ -2,4 +2,15 @@ from sys import argv
 
 LETTERS = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 script, filename = argv
-fileText = open(filename).lower()
+fileObj = open(filename)
+fileContents = fileObj.read()
+fileContents = fileContents.lower()
+fileContents.replace(" ", "")
+
+for letter in LETTERS:
+	occurences = 0
+	for line in fileContents:
+		for char in line:
+			if char == letter:
+				occurences += 1
+	print str(occurences)
